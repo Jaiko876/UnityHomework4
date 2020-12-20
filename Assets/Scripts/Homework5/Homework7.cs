2705dc8590341272381fd03c57f4a26709c18ba1
Молоствов Игорь <Jaiko876@gmail.com>
2020-12-20 13:46:38 +0300

    Homework7

---
+++ Assets/Scripts/Homework5/Homework7.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Homework7 : EditorWindow
+{
+    public static GameObject ObjectInstantiate;
+    private void OnGUI()
+    {
+        GUILayout.Label("Базовые настройки", EditorStyles.boldLabel);
+        ObjectInstantiate =
+            EditorGUILayout.ObjectField("Объект который хотим вставить",
+                    ObjectInstantiate, typeof(GameObject), true)
+                as GameObject;
+        var colorField = EditorGUILayout.ColorField("color", Color.cyan);
+        var button = GUILayout.Button("Применить");
+        if (button)
+        {
+            if (ObjectInstantiate)
+            {
+                var component = ObjectInstantiate.GetComponent<Renderer>();
+                component.sharedMaterial.color = colorField;
+            }
+        }
+    }
+}
+public class MenuItems
+{
+    [MenuItem("Homework7/Homework7")]
+    private static void MenuOption()
+    {
+        EditorWindow.GetWindow(typeof(Homework7), false, "ДЗ 7");
+    }
+}
